74b837ee35fd7f0d79adacba553d1dcd191357ed
FlyingFathead <flyingfathead@protonmail.com>
2024-10-04 15:05:57 +0300

    `v0.1704` - token reading priority & logic

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 # openai-whisper transcriber-bot for Telegram
 
 # version of this program
-version_number = "0.1703"
+version_number = "0.1704"
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # https://github.com/FlyingFathead/whisper-transcriber-telegram-bot/
